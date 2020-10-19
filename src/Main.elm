c2951bebe8415295675b123cdc9a983e44e7dbc0
nick <nick@orb-central.local>
2020-10-19 00:39:55 -0700

    run the autoformatter

--- src/Main.elm
+++ src/Main.elm
@@ -1,46 +1,48 @@
 module Main exposing (..)
 
 import Char
-import Parser exposing (Parser, (|=), (|.))
+import Parser exposing ((|.), (|=), Parser)
 import Result
 import Set
 
-type Expr = Term String
-          | Appl Expr Expr
 
-type Tokens = Symbol String
-            | Parens (List Tokens)
+type Expr
+    = Term String
+    | Appl Expr Expr
+
 
 expr : Parser Expr
 expr =
     singleExpr
         |> Parser.andThen
-           (\e -> Parser.loop e exprHelper)
+            (\e -> Parser.loop e exprHelper)
+
 
 exprHelper : Expr -> Parser (Parser.Step Expr Expr)
 exprHelper soFar =
     Parser.oneOf
         [ Parser.succeed (\next -> Parser.Loop <| Appl soFar next)
-              |= singleExpr
+            |= singleExpr
         , Parser.succeed (Parser.Done soFar)
         ]
+
 
 singleExpr : Parser Expr
 singleExpr =
     Parser.oneOf
         [ Parser.succeed Term
-              |= Parser.variable
-                  { start = Char.isAlpha
-                  , inner = always False
-                  , reserved = Set.empty
-                  }
+            |= Parser.variable
+                { start = Char.isAlpha
+                , inner = always False
+                , reserved = Set.empty
+                }
         , Parser.succeed Term
             |. Parser.symbol "["
             |= Parser.variable
-                   { start = \c -> c /= ']'
-                   , inner = \c -> c /= ']'
-                   , reserved = Set.empty
-                   }
+                { start = \c -> c /= ']'
+                , inner = \c -> c /= ']'
+                , reserved = Set.empty
+                }
             |. Parser.symbol "]"
         , Parser.succeed identity
             |. Parser.symbol "("
@@ -48,5 +50,4 @@ singleExpr =
             |= Parser.lazy (\_ -> expr)
             |. Parser.symbol ")"
         ]
-    |. Parser.spaces
-
+        |. Parser.spaces
