e441dc2167dcad29a2149afe9ab7c5f8343d3e2a
nick <nick@orb-central.local>
2020-10-20 00:47:14 -0700

    basic pattern matching

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,10 @@ module Main exposing (..)
 
 import Char
 import Dict exposing (Dict)
+import List
+import List.Extra as List
+import Maybe
+import Maybe.Extra as Maybe
 import Parser exposing ((|.), (|=), Parser)
 import Result
 import Set
@@ -10,6 +14,13 @@ import Set
 type Expr a
     = Term a String
     | Appl a (Expr a) (Expr a)
+
+
+mapExpr : (a -> b) -> Expr a -> Expr b
+mapExpr f e =
+    case e of
+        Term a t -> Term (f a) t
+        Appl a x y -> Appl (f a) (mapExpr f x) (mapExpr f y)
 
 
 type alias PlainExpr =
@@ -62,3 +73,47 @@ parseExpr : String -> Result String PlainExpr
 parseExpr input =
     Parser.run expr input
         |> Result.mapError Parser.deadEndsToString
+
+
+type TermType
+    = Free
+    | Bound
+
+
+type alias MatchExpr =
+    Expr TermType
+
+
+matchExpr : MatchExpr -> PlainExpr -> Maybe (Dict String PlainExpr)
+matchExpr pattern toMatch =
+    case ( pattern, toMatch ) of
+        ( Term Free t, val ) ->
+            Just <| Dict.singleton t val
+
+        ( Term Bound t1, Term () t2 ) ->
+            if t1 == t2 then
+                Just Dict.empty
+
+            else
+                Nothing
+
+        ( Appl _ x1 y1, Appl () x2 y2 ) ->
+            matchExpr x1 x2
+                |> Maybe.andThen
+                    (\x ->
+                        matchExpr y1 y2
+                            |> Maybe.map (Dict.union x)
+                    )
+
+        ( _, _ ) ->
+            Nothing
+
+
+freeAllTermsButHead : PlainExpr -> MatchExpr
+freeAllTermsButHead e =
+    case e of
+        Term () t -> Term Bound t
+        Appl () x y -> Appl Bound (freeAllTermsButHead x) (mapExpr (\_ -> Free) y)
+
+
+
--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -1,46 +1,121 @@
 module MainTest exposing (..)
 
+import Dict
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import List
+import Main exposing (..)
+import Maybe
+import Maybe.Extra as Maybe
+import Result
+import Result.Extra as Result
 import Test exposing (..)
 
-import Main exposing (..)
 
 suite : Test
 suite =
     describe "main test suite"
-        [ exprSuite ]
+        [ exprSuite
+        ]
+
 
 exprSuite : Test
-
 exprSuite =
     describe "Expr handling"
         [ describe "parser"
-              [ verifyParse
-                   "single term"
-                    "A"
+            [ assertParseResult
+                "single term"
+                "A"
+                (Term () "A")
+            , assertParseResult
+                "single application"
+                "AB"
+                (Appl () (Term () "A") (Term () "B"))
+            , assertParseResult
+                "multiple implicit application"
+                "ABC"
+                (Appl ()
+                    (Appl ()
+                        (Term () "A")
+                        (Term () "B")
+                    )
+                    (Term () "C")
+                )
+            , assertParseResult
+                "multiple explicit application"
+                "A(BC)"
+                (Appl ()
                     (Term () "A")
-              , verifyParse
-                    "single application"
-                    "AB"
-                    (Appl () (Term () "A") (Term () "B"))
-              , verifyParse
-                    "multiple implicit application"
-                    "ABC"
-                    (Appl () (Appl () (Term () "A")
-                                      (Term () "B"))
-                             (Term () "C"))
-              , verifyParse
-                    "multiple explicit application"
-                    "A(BC)"
-                    (Appl () (Term () "A")
-                             (Appl () (Term () "B")
-                                      (Term () "C")))
-              ]
+                    (Appl ()
+                        (Term () "B")
+                        (Term () "C")
+                    )
+                )
+            ]
+        , describe "matching"
+            [ assertMatchExpr
+                "singleton"
+                "A"
+                "A"
+                (Just [])
+            , assertMatchExpr
+                "failed match"
+                "A"
+                "B"
+                Nothing
+            , assertMatchExpr
+                "failed match 2"
+                "A"
+                "AB"
+                Nothing
+            , assertMatchExpr
+                "single bound variable"
+                "Ax"
+                "AB"
+                (Just [ ( "x", "B" ) ])
+            , assertMatchExpr
+                "multiple bound variables"
+                "A(xy)"
+                "A(BC)"
+                (Just [ ( "x", "B" ), ( "y", "C" ) ])
+            , assertMatchExpr
+                "bound subtree"
+                "Axy"
+                "A(BCD)(E(FG))"
+                (Just [ ( "x", "BCD" ), ( "y", "E(FG)" ) ])
+            ]
         ]
 
-verifyParse : String -> String -> PlainExpr -> Test
-verifyParse name input expect =
-    test name <| \_ ->
-       parseExpr input
-           |> Expect.equal (Ok expect)
+
+assertParseResult : String -> String -> PlainExpr -> Test
+assertParseResult name input expect =
+    test name <|
+        \_ ->
+            parseExpr input
+                |> Expect.equal (Ok expect)
+
+
+assertMatchExpr : String -> String -> String -> Maybe (List ( String, String )) -> Test
+assertMatchExpr name pattern toMatch expect =
+    test name <|
+        \_ ->
+            Maybe.andThen2
+                matchExpr
+                (parseExpr pattern
+                    |> Result.map freeAllTermsButHead
+                    |> Result.toMaybe
+                )
+                (parseExpr toMatch |> Result.toMaybe)
+                |> Expect.equal
+                    (expect
+                        |> Maybe.andThen
+                            (List.map
+                                (\( t, e ) ->
+                                    parseExpr e
+                                        |> Result.toMaybe
+                                        |> Maybe.map (\pe -> ( t, pe ))
+                                )
+                                >> Maybe.combine
+                                >> Maybe.map Dict.fromList
+                            )
+                    )
